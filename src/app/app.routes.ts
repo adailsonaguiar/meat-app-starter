a7d5875275b4e55eeb4af4330be2e73da98a2139
adailsonaguiar <adailsonacj@live.com>
2019-06-27 13:12:47 -0300

    adicionando componentes filhos (Rotas filhas)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,19 @@ import { HomeComponent } from './home/home.component';
 import { AbountComponent } from './abount/abount.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent },
     { path: 'about', component: AbountComponent },
     { path: 'restaurants', component: RestaurantsComponent },
-    { path: 'restaurants/:id', component: RestaurantDetailComponent }
+    {
+        path: 'restaurants/:id', component: RestaurantDetailComponent,
+        children: [
+            { path: '', redirectTo: 'menu', pathMatch: 'full' },
+            { path: 'menu', component: MenuComponent },
+            { path: 'reviews', component: ReviewsComponent }
+        ]
+    }
 ]
--- src/app/restaurants/restaurants.services.ts
+++ src/app/restaurants/restaurants.services.ts
@@ -25,7 +25,7 @@ export class RestaurantsService {
     }
 
     reviewsOfRestaurants(id: string): Observable<any> {
-        return this.http.get(`${MEAT_API}/restaurants/${id}/rewiews`)
+        return this.http.get(`${MEAT_API}/restaurants/${id}/reviews`)
             .map(response => response.json())
             .catch(ErrorHandler.handleError)
     }
