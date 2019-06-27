0892b049588c3f883f5a45dacec402cd18d1df3d
adailsonaguiar <adailsonacj@live.com>
2019-06-26 21:51:17 -0300

    Tratamentos de erros e modificação de pasta de arquivo de api

--- src/app/restaurants/restaurant/restaurant.services.ts
+++ src/app/restaurants/restaurant/restaurant.services.ts
@@ -1,10 +1,11 @@
 import { Restaurant } from './restaurant.model'
-import { MEAT_API } from '../app.api'
+import { MEAT_API } from '../../app.api'
 import { Http } from '@angular/http'
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
 import 'rxjs/add/operator/catch'
+import { ErrorHandler } from '../../app.error-handler'
 
 @Injectable()
 export class RestaurantService {
@@ -14,5 +15,6 @@ export class RestaurantService {
     restaurants(): Observable<Restaurant[]> {
         return this.http.get(`${MEAT_API}/restaurants`)
             .map(response => response.json())
+            ._catch(ErrorHandler.handleError)
     }
 }
