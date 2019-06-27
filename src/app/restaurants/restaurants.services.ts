ed4a80c8e2407ec9f66bdceff176102b8313dd2e
adailsonaguiar <adailsonacj@live.com>
2019-06-27 10:01:24 -0300

    alterações em detalhes de restaurantes

--- src/app/restaurants/restaurants.services.ts
+++ src/app/restaurants/restaurants.services.ts
@@ -18,5 +18,10 @@ export class RestaurantsService {
             ._catch(ErrorHandler.handleError)
     }
 
-  
+    restaurantById(id: string): Observable<Restaurant> {
+        return this.http.get(`${MEAT_API}/restaurants/${id}`)
+            .map(response => response.json())
+            .catch(ErrorHandler.handleError)
+    }
+
 }
