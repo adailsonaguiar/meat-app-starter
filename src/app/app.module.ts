9e0e3728c990bc281fe34f1ab847523705ad7282
Adailson Aguiar <adailsonacj@live.com>
2019-08-12 15:46:02 -0300

    validação do formulário de compras

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule, LOCALE_ID } from '@angular/core'
+import { HttpModule } from '@angular/http'
+import { RouterModule } from '@angular/router'
+import { FormsModule } from '@angular/forms'
 
 import { ROUTES } from './app.routes'
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { AbountComponent } from './abount/abount.component';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { AppComponent } from './app.component'
+import { HeaderComponent } from './header/header.component'
+import { HomeComponent } from './home/home.component'
+import { AbountComponent } from './abount/abount.component'
+import { RestaurantsComponent } from './restaurants/restaurants.component'
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
 import { RestaurantsService } from './restaurants/restaurants.services'
-import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { MenuComponent } from './restaurant-detail/menu/menu.component';
-import { CarrinhoComponent } from './restaurant-detail/carrinho/carrinho.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component'
+import { MenuComponent } from './restaurant-detail/menu/menu.component'
+import { CarrinhoComponent } from './restaurant-detail/carrinho/carrinho.component'
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component'
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component'
-import { CarrinhoService } from './restaurant-detail/carrinho/carrinho.service';
-import { OrderComponent } from './order/order.component';
-
+import { CarrinhoService } from './restaurant-detail/carrinho/carrinho.service'
+import { OrderComponent } from './order/order.component'
 
 @NgModule({
   declarations: [
@@ -39,7 +39,8 @@ import { OrderComponent } from './order/order.component';
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    FormsModule
   ],
   providers: [
     RestaurantsService,
